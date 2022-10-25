c0e66d834e278ac368de881ef7d99a49b04b457d
anela <anela89@gmail.com>
2022-10-25 16:44:27 -0700

    Wrote selection sort implementation.

---
+++ dsa-sorting/selection.js
@@ -0,0 +1,39 @@
+"use strict";
+
+/**
+ * Selection Sort Algorithm
+ * 
+ * O(n**2) runtime
+ * 
+ * Ex: Given a list of numbers, return the list sorted biggest to smallest.
+ * 
+ */
+
+
+// Returns index of the biggest item in an array.
+function findIndexOfBiggest(arr) {
+    let biggest = arr[0];
+    let biggestIndex = 0;
+
+    for (let i=0; i<arr.length; i++) {
+        if (arr[i] > biggest) {
+            biggest = arr[i];
+            biggestIndex = i;
+        }
+    }
+
+    return biggestIndex;
+}
+
+
+function selectionSort(arr) {
+ let sortedArr = [];
+ 
+ while (arr.length > 0) {
+    let indexOfBiggest = findIndexOfBiggest(arr);
+    sortedArr.push(arr[indexOfBiggest]);
+    arr.splice(indexOfBiggest, 1);
+ }
+
+ return sortedArr;
+}
