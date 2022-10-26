43a5117ae87f179e647374ea9dfe54a9641331a0
anela <anela89@gmail.com>
2022-10-26 15:05:28 -0700

    Wrote implementation of binary search algorithm. Updated tests to reflect requirements.

--- dsa-misc/binarysearch.js
+++ dsa-misc/binarysearch.js
@@ -5,12 +5,28 @@
  * 
  * O(log2 n)
  * 
- * Takes a sorted list and an item, returns sought item or null.
+ * Takes a sorted list and a sought value, returns index of value or -1.
  * 
  */
 
-function binarySearch() {
+function binarySearch(arr, val) {
+  let low = 0;
+  let high = arr.length-1;
 
+  while (low <= high) {
+    let mid = Math.floor((low+high)/2);
+    let guess = arr[mid];
+
+    if (guess === val) {
+      return mid;
+    } else if (guess > val) {
+      high = mid-1;
+    } else {
+      low = mid+1;
+    }
+  }
+
+  return -1;
 }
 
-module.export = binarySearch;
+module.exports = binarySearch;
--- dsa-misc/binarysearch.test.js
+++ dsa-misc/binarysearch.test.js
@@ -4,38 +4,38 @@ const binarySearch = require("./binarysearch")
 
 describe('binarySearch', function() {
     it('should exist', function() {
-        expect(typeof binarySearch).toBe(
-            'function',
-            "did you remember to define the 'binarySearch function?"
-        );
+      expect(typeof binarySearch).toBe(
+        'function',
+        "did you remember to define the 'binarySearch function?"
+      );
     });
 
     it('should return sought value', function() {
       expect(binarySearch([4, 7, 9, 10, 12, 20], 9)).toEqual(
-        9,
+        2,
         "binarySearch([4, 7, 9, 10, 12, 20], 9) should equal 9"
       );
 
       expect(binarySearch([-10, 0, 4, 7], 7)).toEqual(
-        7,
+        3,
         "binarySearch([-10, 0, 4, 7], 7) should equal 7"
       );
 
       expect(binarySearch([1, 2, 3], 1)).toEqual(
-        1,
+        0,
         "binarySearch([1, 2, 3], 1) should equal 1"
       );
     });
 
-    it('should return null', function() {
+    it('should return -1', function() {
       expect(binarySearch([], 1)).toEqual(
-        null,
-        "binarySearch([], 1) should equal null"
+        -1,
+        "binarySearch([], 1) should equal -1"
       );
 
       expect(binarySearch([1, 2, 3], 4)).toEqual(
-        null,
-        "binarySearch([1, 2, 3,], 4) should equal null"
+        -1,
+        "binarySearch([1, 2, 3,], 4) should equal -1"
       );
     });
 });
