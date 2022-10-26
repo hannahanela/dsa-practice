40b37aa1edc0e2885989bcbeb5e390b89da51748
anela <anela89@gmail.com>
2022-10-26 09:35:40 -0700

    Started binary search algorithm.

---
+++ dsa-misc/binarysearch.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/**
+ * Binary Search Algorithm
+ * 
+ * O(log2 n)
+ * 
+ * Takes a sorted list and an item, returns sought item or null.
+ * 
+ */
+
+function binarySearch() {
+
+}
+
+modules.export = binarySearch;
